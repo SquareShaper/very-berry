45b3e3d3ef868cd78f1a6e626e3b8644dff76d40
SquareShaper <squareshaper@tuta.io>
2026-07-28 22:45:16 +0200

    make the bundled thornberry chips return a bundle (sadly not the correct color...)

--- src/main/java/net/squareshaper/veryberry/item/BundledThornberryChips.java
+++ src/main/java/net/squareshaper/veryberry/item/BundledThornberryChips.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.world.World;
 
 public class BundledThornberryChips extends Item {
@@ -15,7 +16,11 @@ public class BundledThornberryChips extends Item {
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
         super.finishUsing(stack.copy(), world, user); // Consume a copy of the stack, so the real one doesn't get removed from inventory
         if (user instanceof PlayerEntity player) {
+            if (stack.getDamage() == stack.getMaxDamage()-1) {
+                stack = new ItemStack(Items.BUNDLE);
+            } else {
             stack.damage(1, player); // damage the real stack
+            }
         }
         return stack; // return the newly damaged stack
     }
